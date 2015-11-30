44a96b99e34aee32ce348b5cc4434d7c75204696
Isaac-V <isaac.vawter@gmail.com>
2015-11-30 12:32:18 -0500

    Added eclipse workspace

---
+++ workspace1.8/Test/src/test/Repl.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Repl {
+
+	public static void main(String[] args) {
+		String a = "the lion, the witch, and the wardrobe";
+		String b = a.replace(' ', '+');
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
